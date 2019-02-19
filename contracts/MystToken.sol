62d83bd01a7142802a2fd1ebda8d6edb11215ca7
Jaro <jaro@nous.lt>
2019-02-19 08:09:52 +0200

    Update token into ERC777

--- contracts/MystToken.sol
+++ contracts/MystToken.sol
@@ -1,11 +1,58 @@
-pragma solidity ^0.5.0;
+pragma solidity >=0.4.25 <0.6.0;
 
-//The purpose of this contract is to have sample ERC20 a la "MYST" compatible mintable token
-//for testing purposes of ERC20 interactions, actually we can use MintableToken directly,
-//but for the sake of demonstrating that any compatible token will do
-import "./deps/OpenZeppelin/contracts/token/ERC20/ERC20Mintable.sol";
+import { Ownable } from "openzeppelin-solidity/contracts/ownership/Ownable.sol";
+import { ERC777ERC20BaseToken } from "./erc777/ERC777ERC20BaseToken.sol";
 
+//The purpose of this contract is to have sample of potential updated "MYST" into ERC777
+//for testing purposes of easier than ERC777 interactions. If this will work well in testnet,
+//we can think of migration of mainnet token into ERC777.
 
-contract MystToken is ERC20Mintable {
+contract MystToken is ERC777ERC20BaseToken, Ownable {
+	event ERC20Enabled();
+    event ERC20Disabled();
 
+	address private mBurnOperator;
+
+	// NOTE: IdentityRegistry should be set as default operator.
+	constructor(
+        string _name,
+        string _symbol,
+        uint256 _granularity,
+        address[] _defaultOperators,
+        address _burnOperator
+    ) public ERC777ERC20BaseToken(_name, _symbol, _granularity, _defaultOperators) {
+        mBurnOperator = _burnOperator;
+    }
+
+    function disableERC20() public onlyOwner {
+        mErc20compatible = false;
+        setInterfaceImplementation("ERC20Token", 0x0);
+        emit ERC20Disabled();
+    }
+
+    function enableERC20() public onlyOwner {
+        mErc20compatible = true;
+        setInterfaceImplementation("ERC20Token", this);
+        emit ERC20Enabled();
+    }
+
+    function mint(address _tokenHolder, uint256 _amount, bytes _operatorData) public onlyOwner {
+        requireMultiple(_amount);
+        mTotalSupply = mTotalSupply.add(_amount);
+        mBalances[_tokenHolder] = mBalances[_tokenHolder].add(_amount);
+
+        callRecipient(msg.sender, 0x0, _tokenHolder, _amount, "", _operatorData, true);
+
+        emit Minted(msg.sender, _tokenHolder, _amount, _operatorData);
+        if (mErc20compatible) { emit Transfer(0x0, _tokenHolder, _amount); }
+    }
+
+    function burn(uint256 _amount, bytes _data) public onlyOwner {
+        super.burn(_amount, _data);
+    }
+
+    function operatorBurn(address _tokenHolder, uint256 _amount, bytes _data, bytes _operatorData) public {
+        require(msg.sender == mBurnOperator, "Not a burn operator");
+        super.operatorBurn(_tokenHolder, _amount, _data, _operatorData);
+    }
 }
