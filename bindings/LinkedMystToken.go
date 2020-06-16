2424f8fcf25761e7c9e8f09846fdb121a35167cd
Viktoras Kuznecovas <viktoras@kuznecovas.net>
2020-06-16 11:08:07 +0300

    Remove unneeded deps

--- bindings/LinkedMystToken.go
+++ bindings/LinkedMystToken.go
@@ -17,14 +17,56 @@
 package bindings
 
 import (
+	"fmt"
+	"regexp"
+	"strings"
+
+	"github.com/ethereum/go-ethereum/accounts/abi"
 	"github.com/ethereum/go-ethereum/accounts/abi/bind"
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/core/types"
-	"github.com/joincivil/go-common/pkg/eth"
 )
 
 // DeployLinkedMystToken deploys the myst token with the provided links.
 // Links should come in the map, via a format "Name":address.
 func DeployLinkedMystToken(opts *bind.TransactOpts, backend bind.ContractBackend, links map[string]common.Address) (common.Address, *types.Transaction, *bind.BoundContract, error) {
-	return eth.DeployContractWithLinks(opts, backend, MystTokenABI, MystTokenBin, links)
+	return DeployContractWithLinks(opts, backend, MystTokenABI, MystTokenBin, links)
+}
+
+// taken from https://github.com/joincivil/go-common/blob/5632d3b02d1561aebbe08085df8b8e1ed3c6a273/pkg/eth/utils.go
+
+// ABILinkLibrary replaces references to a library
+// with the actual addresses to those library contracts
+func ABILinkLibrary(bin string, libraryName string, libraryAddress common.Address) string {
+	libstr := fmt.Sprintf("_+%v_+", libraryName)
+	libraryRexp := regexp.MustCompile(libstr)
+
+	// Remove the 0x prefix from those addresses, just need the actual hex string
+	cleanLibraryAddr := strings.Replace(libraryAddress.Hex(), "0x", "", -1)
+
+	modifiedBin := libraryRexp.ReplaceAllString(bin, cleanLibraryAddr)
+
+	return modifiedBin
+}
+
+// DeployContractWithLinks patches a contract bin with provided library addresses
+func DeployContractWithLinks(
+	opts *bind.TransactOpts,
+	backend bind.ContractBackend,
+	abiString string,
+	bin string,
+	libraries map[string]common.Address,
+	params ...interface{},
+) (common.Address, *types.Transaction, *bind.BoundContract, error) {
+
+	for libraryName, libraryAddress := range libraries {
+		bin = ABILinkLibrary(bin, libraryName, libraryAddress)
+	}
+
+	parsed, err := abi.JSON(strings.NewReader(abiString))
+	if err != nil {
+		return common.Address{}, nil, nil, err
+	}
+
+	return bind.DeployContract(opts, parsed, common.FromHex(bin), backend, params...)
 }
--- go.mod
+++ go.mod
@@ -3,14 +3,28 @@ module github.com/mysteriumnetwork/payments
 go 1.13
 
 require (
+	github.com/aristanetworks/goarista v0.0.0-20190325233358-a123909ec740 // indirect
+	github.com/btcsuite/btcd v0.0.0-20190523000118-16327141da8c // indirect
+	github.com/cespare/cp v1.1.1 // indirect
+	github.com/deckarep/golang-set v1.7.1 // indirect
+	github.com/edsrzf/mmap-go v1.0.0 // indirect
 	github.com/ethereum/go-ethereum v1.9.15
+	github.com/fjl/memsize v0.0.0-20190710130421-bcb5799ab5e5 // indirect
+	github.com/go-kit/kit v0.9.0 // indirect
+	github.com/go-logfmt/logfmt v0.4.0 // indirect
+	github.com/golang/protobuf v1.3.2 // indirect
+	github.com/google/uuid v1.1.1 // indirect
 	github.com/gorilla/websocket v1.4.1 // indirect
-	github.com/joincivil/go-common v0.0.0-20191010210240-88ecdf41b6b6
 	github.com/magefile/mage v1.8.0
+	github.com/mattn/go-colorable v0.1.2 // indirect
 	github.com/mysteriumnetwork/go-ci v0.0.0-20200415074834-39fc864b0ed4
+	github.com/pborman/uuid v0.0.0-20180906182336-adf5a7427709 // indirect
 	github.com/pkg/errors v0.8.1
 	github.com/prometheus/tsdb v0.10.0 // indirect
+	github.com/rjeczalik/notify v0.9.2 // indirect
 	github.com/rs/cors v1.7.0 // indirect
 	github.com/rs/zerolog v1.17.2
+	github.com/status-im/keycard-go v0.0.0-20190424133014-d95853db0f48 // indirect
 	github.com/stretchr/testify v1.4.0
+	github.com/tyler-smith/go-bip39 v1.0.2 // indirect
 )
