57e8e79fb1c055a33efea367b777aed8f9e9ee82
Viktoras Kuznecovas <viktoras@kuznecovas.net>
2020-07-30 13:45:09 +0300

    New token

--- gen.go
+++ gen.go
@@ -23,4 +23,4 @@ package bindings
  * looked up in migration script here: https://github.com/mysteriumnetwork/payments-smart-contracts/blob/master/migrations/2_deploy_contracts.js
  */
 
-//go:generate go run bindings/abi/abigen.go --githubrepo=mysteriumnetwork/payments-smart-contracts -githubrelease=v0.9.9 --contracts=SafeMathLib.json,TestMystToken.json,MystDEX.json,ChannelImplementation.json,Registry.json,HermesContract.json,HermesImplementation.json --out=bindings --pkg=bindings
+//go:generate go run bindings/abi/abigen.go --githubrepo=mysteriumnetwork/payments-smart-contracts -githubrelease=v0.10.0 --contracts=SafeMathLib.json,TestMystToken.json,MystDEX.json,ChannelImplementation.json,Registry.json,HermesContract.json,HermesImplementation.json --out=bindings --pkg=bindings
