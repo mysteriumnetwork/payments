0f248233ebb78f525760ecaba1b692b3eb6d175a
Guillem <guillem@mysterium.network>
2021-09-13 09:32:16 +0200

    Refactor stations to simplify usage
    Signed-off-by: Guillem <guillem@mysterium.network>
    
    finished refactor of stations
    
    Signed-off-by: Guillem <guillem@mysterium.network>
    
    fixed errors from review
    
    Signed-off-by: Guillem <guillem@mysterium.network>
    
    forgot one change
    
    Signed-off-by: Guillem <guillem@mysterium.network>
    
    review fixes
    
    Signed-off-by: Guillem <guillem@mysterium.network>

--- transfer/gas/etherscan.go
+++ transfer/gas/etherscan.go
@@ -2,7 +2,6 @@ package gas
 
 import (
 	"encoding/json"
-	"errors"
 	"fmt"
 	"io/ioutil"
 	"math/big"
@@ -20,8 +19,8 @@ const DefaultEtherscanEndpointURI = "https://api.etherscan.io/"
 
 // EtherscanStation represents the etherscan api to retrive gas prices.
 type EtherscanStation struct {
-	APIKey      string
-	EndpointURI string
+	apiKey      string
+	endpointURI string
 	upperBound  *big.Int
 
 	client *http.Client
@@ -38,54 +37,43 @@ func NewEtherscanStation(apiKey, endpointURI string, upperBound *big.Int) *Ether
 		client: &http.Client{
 			Timeout: 10 * time.Second,
 		},
-		EndpointURI: endpointURI,
+		endpointURI: endpointURI,
 		upperBound:  upperBound,
-		APIKey:      apiKey,
+		apiKey:      apiKey,
 	}
 }
 
-func (esa *EtherscanStation) GetAverageGasPrice() (*big.Int, error) {
+func (esa *EtherscanStation) GetGasPrices() (*GasPrices, error) {
 	res, err := esa.request()
 	if err != nil {
 		return nil, err
 	}
-	value, err := strconv.ParseFloat(res.Result.ProposeGasPrice, 64)
+	average, err := strconv.ParseFloat(res.Result.ProposeGasPrice, 64)
 	if err != nil {
 		return nil, err
 	}
-	return esa.result(value), nil
-}
-
-func (esa *EtherscanStation) GetLowGasPrice() (*big.Int, error) {
-	res, err := esa.request()
+	safeLow, err := strconv.ParseFloat(res.Result.SafeGasPrice, 64)
 	if err != nil {
 		return nil, err
 	}
-	value, err := strconv.ParseFloat(res.Result.SafeGasPrice, 64)
+	fast, err := strconv.ParseFloat(res.Result.FastGasPrice, 64)
 	if err != nil {
 		return nil, err
 	}
-	return esa.result(value), nil
-}
-
-func (dpa *EtherscanStation) GetFastGasPrice() (*big.Int, error) {
-	res, err := dpa.request()
-	if err != nil {
-		return nil, err
+	prices := GasPrices{
+		SafeLow: esa.result(safeLow),
+		Average: esa.result(average),
+		Fast:    esa.result(fast),
 	}
-	value, err := strconv.ParseFloat(res.Result.FastGasPrice, 64)
-	if err != nil {
-		return nil, err
-	}
-	return dpa.result(value), nil
+	return &prices, nil
 }
 
 func (esa *EtherscanStation) request() (*etherscanGasPriceResponse, error) {
-	if esa.APIKey == "" {
+	if esa.apiKey == "" {
 		log.Warn().Msg("no API key set, rate is limited")
 	}
 
-	response, err := esa.client.Get(fmt.Sprintf("%v%v%v", esa.EndpointURI, "api?module=gastracker&action=gasoracle&apikey=", esa.APIKey))
+	response, err := esa.client.Get(fmt.Sprintf("%v%v%v", esa.endpointURI, "api?module=gastracker&action=gasoracle&apikey=", esa.apiKey))
 	if err != nil {
 		return nil, err
 	}
@@ -96,16 +84,14 @@ func (esa *EtherscanStation) request() (*etherscanGasPriceResponse, error) {
 		return nil, err
 	}
 
-	fmt.Println(string(resp))
-
 	var res etherscanGasPriceResponse
 	err = json.Unmarshal(resp, &res)
 	if err != nil {
-		return nil, err
+		return nil, fmt.Errorf("failed to read response body from etherscan with an error: %w and body: %s", err, string(resp))
 	}
 
 	if res.Status != "1" {
-		return nil, errors.New("etherscan API request failed")
+		return nil, fmt.Errorf("etherscan api failed with message: %s", res.Message)
 	}
 
 	return &res, nil
@@ -121,6 +107,12 @@ type etherscanGasPriceResponse struct {
 	Status  string         `json:"status"`
 	Message string         `json:"message"`
 	Result  gasPriceResult `json:"result"`
+}
+
+type etherscanGasPriceResponseFail struct {
+	Status  string `json:"status"`
+	Message string `json:"message"`
+	Result  string `json:"result"`
 }
 
 // gasPriceResult the gas prices for the last block.
