ed65c1bc21b51c5b98a69b4e3652ab0548a9d5ac
Guillem <guillem@mysterium.network>
2021-09-23 11:50:59 +0200

    made multichainStation multiStation
    Signed-off-by: Guillem <guillem@mysterium.network>
    
    added tests for multichain station
    
    Signed-off-by: Guillem <guillem@mysterium.network>

--- transfer/gas/multichain_station.go
+++ transfer/gas/multichain_station.go
@@ -2,17 +2,28 @@ package gas
 
 import (
 	"fmt"
+
+	"github.com/rs/zerolog/log"
 )
 
 // MultichainStation is a station that can hold multiple station
 // and call them depending on the chain given.
-type MultichainStation map[int64]Station
+type MultichainStation map[int64][]Station
 
 func (m MultichainStation) GetGasPrices(chainID int64) (*GasPrices, error) {
-	station, ok := m[chainID]
+	stations, ok := m[chainID]
 	if !ok {
-		return nil, fmt.Errorf("no gas station for chain %d", chainID)
+		return nil, fmt.Errorf("no gas stations for chain %d", chainID)
 	}
 
-	return station.GetGasPrices()
+	for i, station := range stations {
+		prices, err := station.GetGasPrices()
+		if err != nil {
+			log.Err(err).Int64("chainID", chainID).Int("stationIndex", i).Msg("failed to get gas prices")
+			continue
+		}
+		return prices, nil
+	}
+
+	return nil, fmt.Errorf("all gas station for chain %d failed", chainID)
 }
