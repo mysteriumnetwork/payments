1bc4f7bc5ee99bb68f6c909dcea67b551b68caa9
Andrey <andrey@mysterium.network>
2022-05-17 11:57:29 +0300

    Add gas station, that do call to bc to get suggested gas price (#205)
    * Add gas station, that do call to bc to get suggested gas price
    * Remove extra call to get latest block number

---
+++ transaction/gas/node_station.go
@@ -0,0 +1,44 @@
+package gas
+
+import (
+	"github.com/ethereum/go-ethereum/consensus/misc"
+	"github.com/ethereum/go-ethereum/core/types"
+	"github.com/ethereum/go-ethereum/params"
+	"math/big"
+)
+
+type NodeStation struct {
+	bc      BCClient
+	chainID int64
+}
+
+func NewNodeStation(bc BCClient, chainID int64) *NodeStation {
+	return &NodeStation{bc: bc, chainID: chainID}
+}
+
+type BCClient interface {
+	SuggestGasPrice(chainID int64) (*big.Int, error)
+	HeaderByNumber(chainID int64, number *big.Int) (*types.Header, error)
+	BlockNumber(chainID int64) (uint64, error)
+}
+
+func (n *NodeStation) GetGasPrices() (*GasPrices, error) {
+	suggestGasPrice, err := n.bc.SuggestGasPrice(n.chainID)
+	if err != nil {
+		return nil, err
+	}
+
+	header, err := n.bc.HeaderByNumber(n.chainID, nil)
+	if err != nil {
+		return nil, err
+	}
+	config := params.MainnetChainConfig
+	baseFee := misc.CalcBaseFee(config, header)
+
+	return &GasPrices{
+		SafeLow: suggestGasPrice,
+		Average: suggestGasPrice,
+		Fast:    suggestGasPrice,
+		BaseFee: baseFee,
+	}, nil
+}
